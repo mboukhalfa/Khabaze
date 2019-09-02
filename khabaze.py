a61478e754b2d5407352cb1de0a9d186f35a8b54
mboukhalfa <mohammed.boukhalfa@aalto.fi>
2019-09-02 15:46:18 +0300

    Rename python3 *.so outputs & option for compiled root

--- khabaze.py
+++ khabaze.py
@@ -4,7 +4,7 @@ import shutil
 import getopt
 EXCLUDE = [ 'khabaze.py', 'Auto_fill.py','clean_osm.py','apps', '__pycache__', '.git' ]
 DONT_COMPILE = [ 'anastacia', 'api', 'manage.py', 'resource_deamon.py' ]
-report = {"errors":"", "success":""}
+errors = ''
 
 def generate_folder (path, name):
     i=1
@@ -17,24 +17,36 @@ def generate_folder (path, name):
         return name
 
 def ila_makhbaza(source_root, path, compiled_root, file_py, makhbaza_root):
+
+    def get_v3_output ():
+
+        for x in os.listdir('./'):
+            if x.startswith(file_py[:-3]) and x.endswith('.so'):
+                return x
+
     shutil.copyfile(source_root + path + file_py,source_root + makhbaza_root + file_py)
     cwd = os.getcwd()
     os.chdir(source_root + makhbaza_root)
+
     v = '3'
     if sys.version_info[0] < 3:
-        v = ''
+        v = '2'
     cmd = 'python{} khabze.py '.format(v) + file_py[:-3]+' build_ext --inplace'
+
     try:
         os.system(cmd)
+        output = file_py[:-3] + '.so' if v  == '2' else get_v3_output()
         os.chdir(cwd)
-        os.rename(source_root + makhbaza_root + file_py[:-3] + '.so',compiled_root + path + file_py[:-3] + '.so')
+        os.rename(source_root + makhbaza_root + output, compiled_root + path + file_py[:-3] + '.so')
     except Exception as e:
-        report['errors'] += 'error in ' + path + file_py + '\n'
+        #errors += 'error in ' + path + file_py + '\n'
         print ('Compilation Error {} '.format(e))
         os.chdir(cwd)
         os.rename(source_root + makhbaza_root + file_py,compiled_root + path + file_py)
 
 def khabaze(source_root,path, compiled_root, makhbaza_root):
+
+
     for x in os.listdir(source_root + path):
         if x in EXCLUDE:
             print(str(x) + ' is excluded')
@@ -54,12 +66,13 @@ def khabaze(source_root,path, compiled_root, makhbaza_root):
         else: shutil.copyfile(source_root + path + x ,compiled_root + path + x)
 
 if __name__ == '__main__':
-    args, _ = getopt.getopt(sys.argv[1:],'hs:m:e:d:',['help','source_root=','main=', 'exclude=','dont_compile='])
+
+    args, _ = getopt.getopt(sys.argv[1:],'hs:m:c:e:d:',['help','source_root=','main=', 'compiled_root=','exclude=','dont_compile='])
     source_root = './'
     name = 'anastacia'
     for flag, arg in args:
         if flag in ('-h', '--help'): 
-            print ('khabaze.py -s <source_root> -m <main_script> -e <exclude> -d <dont_compile>')
+            print ('khabaze.py -s <source_root> -m <main_script> -c <compiled_root> -e <exclude> -d <dont_compile>')
             sys.exit()
         if flag in ('-s', '--source_root'): 
             if os.path.isdir(arg.split(',')[0]):
@@ -71,7 +84,9 @@ if __name__ == '__main__':
             else: raise Exception ('Invalid main script file')
             DONT_COMPILE.append(main)
             name = os.path.splitext(name)[0]
-    #    if flag in ('-c', '--compiled_root'): compiled_root = arg.split(',')
+        if flag in ('-c', '--compiled_root'):
+            if os.path.isdir(arg.split(',')[0]):
+                compiled_rootf = arg.split(',')[0]
         if flag in ('-e', '--exclude'): EXCLUDE += arg.split(',')
         if flag in ('-d', '--dont_compile'): DONT_COMPILE += arg.split(',')
 
@@ -98,5 +113,7 @@ setup(
     os.mkdir(source_root + compiled_root)
     khabaze(source_root, '', source_root + compiled_root + '/', makhbaza_root + '/')
     shutil.rmtree(source_root + makhbaza_root)
+    shutil.move(source_root+ compiled_root, os.path.abspath(compiled_rootf))
+
     print ('--------------- Report ---------------')
-    print(report['errors'])
+    print(errors)
